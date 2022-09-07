a0c1e678d06f6dc4b7b2430d424c5efc92a59d35
khaled farghly <khaledfarghly9991@gmail.com>
2022-09-07 08:18:14 +0200

    setup & test react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/designsystem'>Design System</Link>
+        </li>
+        <li>
+          <Link to='/destinations'>Destinations</Link>
+        </li>
+        <li>
+          <Link to='/crew'>Crew</Link>
+        </li>
+        <li>
+          <Link to='/technology'>Technology</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path='/' element={<Navigate to='/home' replace={true} />} />
+        <Route path='/home' element={<div>welcome to home</div>} />
+        <Route
+          path='/designsystem'
+          element={<div>welcome to design system</div>}
+        />
+
+        <Route path='/destinations' element={<p>welcome to destinations</p>} />
+        <Route path='/crew' element={<div>welcome to Crew</div>} />
+        <Route path='/technology' element={<div>welcome to Technology </div>} />
+
+        <Route path='*' element={<div> Not Found </div>} />
+      </Routes>
     </div>
   );
 }
