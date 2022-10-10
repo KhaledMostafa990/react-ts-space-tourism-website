df8a81804f96311fa18ab8b77a4c2a54ac4370ba
khaled farghly <khaledfarghly9991@gmail.com>
2022-10-10 04:46:41 +0200

    fix bug and refactoring animation for inner pages content

--- src/components/base/NumberedHeading.tsx
+++ src/components/base/NumberedHeading.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NumberedTitle } from './NumberedTitle';
-
+import { motion } from 'framer-motion';
 export function NumberedHeading({
   num,
   text,
@@ -11,12 +11,31 @@ export function NumberedHeading({
   classes?: string;
 }) {
   return (
-    <h1 className={`fs-500 ${classes}`}>
+    <motion.h1
+      className={`fs-500 ${classes}`}
+      variants={toBottom}
+      initial='invisible'
+      animate='visible'
+    >
       <NumberedTitle
         num={num}
         text={text}
         numClasses='text-primary-300 pr-1 '
       />
-    </h1>
+    </motion.h1>
   );
 }
+const toBottom = {
+  invisible: {
+    opacity: 0,
+    y: -75,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      duration: 3,
+    },
+  },
+};
